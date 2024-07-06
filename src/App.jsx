3de30962f602c4754edf92ba59064111c4acaa56
Bad-Creator <syryoga1@gmail.com>
2024-07-06 23:58:39 +0200

    navigation for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Services from "./components/Services/Services";
 import Resume from "./components/Resume/Resume";
+import React, { useState } from "react";
 
 export default function App() {
+  const [activeComponent, setActiveComponent] = useState("home");
   return (
     <>
       <div className="conntainer">
-        <Navbar></Navbar>
-        <Home></Home>
-        {/* <Services /> */}
-        {/* <Resume /> */}
+        <Navbar
+          setActiveComponent={setActiveComponent}
+          activeComponent={activeComponent}
+        />
+        {activeComponent === "home" && <Home />}
+        {activeComponent === "services" && <Services />}
+        {activeComponent === "resume" && <Resume />}
       </div>
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import "./Navbar.css";
 
-export default function Navbar() {
+export default function Navbar({ setActiveComponent, activeComponent }) {
+  const handleNavClick = (component) => {
+    setActiveComponent(component);
+  };
   return (
     <nav className="navbar">
       <div className="logo">
@@ -10,11 +13,36 @@ export default function Navbar() {
       </div>
       <div className="navlist">
         <ul>
-          <li>home</li>
-          <li>services</li>
-          <li>resume</li>
-          <li>work</li>
-          <li>contact</li>
+          <li
+            className={activeComponent === "home" ? "active" : ""}
+            onClick={() => handleNavClick("home")}
+          >
+            home
+          </li>
+          <li
+            className={activeComponent === "services" ? "active" : ""}
+            onClick={() => handleNavClick("services")}
+          >
+            services
+          </li>
+          <li
+            className={activeComponent === "resume" ? "active" : ""}
+            onClick={() => handleNavClick("resume")}
+          >
+            resume
+          </li>
+          {/* <li
+            className={activeComponent === "home" ? "active" : ""}
+            onClick={() => handleNavClick("home")}
+          >
+            work
+          </li>
+          <li
+            className={activeComponent === "home" ? "active" : ""}
+            onClick={() => handleNavClick("home")}
+          >
+            contact
+          </li> */}
         </ul>
         <button className="btn">Hire me</button>
       </div>
