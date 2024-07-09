a9e6c7171a8e9190093064608b9c3ebdef1676da
sergheipascal <syryoga1@gmail.com>
2024-07-09 17:22:14 +0200

    Completed Work page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Contact() {
   return <div>Contact page</div>;
 }
